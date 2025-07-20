8516e0bf458fbec6ed3063272e73e8bd808a298a
Manish Kumar <99637253+kumarmanishiitm@users.noreply.github.com>
2025-07-20 17:55:58 +0530

    Update vite.config.js

--- neet-test-frontend/vite.config.js
+++ neet-test-frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://zerotohero-1.onrender.com',
         changeOrigin: true
       }
     }
